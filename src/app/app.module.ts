4002d0ac7b208c4d6c165c69589c95ecd1e0e18a
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-06-01 23:55:27 +0530

    resolve error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppRouterModule} from "./app-router.module";
+import {MatToolbarModule} from "@angular/material";
+import {ListComponent} from "./components/list/list.component";
+import {EditComponent} from "./components/edit/edit.component";
+import {CreateComponent} from "./components/create/create.component";
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRouterModule
+    AppRouterModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
