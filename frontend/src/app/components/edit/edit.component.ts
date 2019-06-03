f0edddff9e0e6af710bd2708039cefe2e5d87cca
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-06-03 17:11:51 +0530

    done

--- frontend/src/app/components/edit/edit.component.ts
+++ frontend/src/app/components/edit/edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {IssueService} from "../../issue.service";
+import {IssueService} from '../../issue.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-edit',
@@ -8,9 +11,44 @@ import {IssueService} from "../../issue.service";
 })
 export class EditComponent implements OnInit {
 
-  constructor(private issueService: IssueService) { }
+  id: string;
+  issue: any = {};
+  updateForm: FormGroup;
+
+  constructor(private issueService: IssueService, private router: Router, private route: ActivatedRoute,
+              private snakbar: MatSnackBar, private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.updateForm = this.fb.group({
+      title: ['', Validators.required],
+      responsible: '',
+      description: '',
+      severity: '',
+      status: ''
+    });
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params.id;
+      this.issueService.getIssueById(this.id).subscribe(res => {
+        this.issue = res;
+        this.updateForm.get('title').setValue(this.issue.title);
+        this.updateForm.get('responsible').setValue(this.issue.responsible);
+        this.updateForm.get('description').setValue(this.issue.description);
+        this.updateForm.get('severity').setValue(this.issue.severity);
+        this.updateForm.get('status').setValue(this.issue.status);
+      });
+    });
+  }
+
+  updateIssue(title, responsible, description, severity, status) {
+    this.issueService.updateIssue(this.id, title, responsible, description, severity, status)
+      .subscribe(() => {
+        this.snakbar.open('Issue updated successfully!', 'OK', {duration: 3000});
+      });
   }
 
 }
